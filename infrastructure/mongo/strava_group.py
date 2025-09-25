83e7cad3ec04168d66504618e7ce2f3d1be3190e
Reno Viana <renotviana@gmail.com>
2025-09-25 11:00:14 -0300

    Remover campos de atividades ignoradas da classe StravaGroup

--- infrastructure/mongo/strava_group.py
+++ infrastructure/mongo/strava_group.py
@@ -3,8 +3,6 @@ from mongoengine import Document, IntField, DictField, ListField
 
 class StravaGroup(Document):
     telegram_group_id = IntField()
-    ignored_activities = ListField()
-    ignored_activities_rank = ListField()
     metas = DictField(default={})
     membros = DictField(default={})
     segments_ids = ListField(required=False, default=[])
